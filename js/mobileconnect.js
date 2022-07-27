3b3016eaf334e4e467954774050b46c7c1cf2d87
eckdqja <46389810+eckdqja@users.noreply.github.com>
2022-07-27 12:43:30 +0900

    Add files via upload

---
+++ js/mobileconnect.js
@@ -0,0 +1,10 @@
+var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android',
+                     'Windows CE', 'Windows CE;', 'LG', 'SAMSUNG',
+                     'MOT', 'SonyEricsson', 'Mobile', 'Symbian',
+                     'Opera Mobi', 'Opera Mini', 'IEmobile');
+            for (var word in mobileKeyWords){
+                if (navigator.userAgent.match(mobileKeyWords[word]) != null){
+                          window.location.href = "./m/l";
+                    break;
+                      }
+            }
